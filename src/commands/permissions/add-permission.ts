ad40682a505ed46753d83df771c88ee463a5f3d7
AuroraEchoes <auroraesmeralda@protonmail.com>
2026-02-24 18:07:45 +1100

    Add embeds: /add-permission

--- src/commands/permissions/add-permission.ts
+++ src/commands/permissions/add-permission.ts
@@ -1,8 +1,9 @@
-import { ApplicationCommandOptionType } from "discord.js";
+import { ApplicationCommandOptionType, EmbedBuilder } from "discord.js";
 import type { Command } from '../index.js';
 import { Permission, PermissionManager } from "../../permissions.js";
 import { PermissionRoles } from "../../db/permission-roles.js";
 import { permissionChoices } from "./permission-utils.js";
+import { failureEmbed, successEmbed } from "../../lib/embeds.js";
 
 export default {
   data: {
@@ -34,15 +35,35 @@ export default {
       .map(role => role.role_id)
       .includes(BigInt(role.id))
     if (alreadyHasRole) {
-      interaction.reply(`Role <@&${role.id}> already has permission ${permission}`)
+      await interaction.reply({ embeds: [embedAlreadyHasPermission(permission.toString(), role.id)], ephemeral: true });
     }
     else {
       const res = await PermissionRoles.addPermissionToRole(permission, BigInt(role.id))
       if (res === undefined) {
+        await interaction.reply({ embeds: [embedFailure(permission.toString(), role.id)], ephemeral: true });
         interaction.reply(`Error giving <@&${role.id}> permission ${permission}`)
       } else {
-        interaction.reply(`Gave <@&${role.id}> permission ${permission}`)
+        await interaction.reply({ embeds: [embedSuccess(permission.toString(), role.id)], ephemeral: true });
       }
     }
   },
 } satisfies Command;
+
+function embedSuccess(permission: string, roleId: string): EmbedBuilder {
+  return successEmbed
+    .setTitle("Permission added to role")
+    .setDescription(`<@&${roleId}> now has permission ${permission}`)
+}
+
+function embedFailure(permission: string, roleId: string): EmbedBuilder {
+  return failureEmbed
+    .setTitle("Error adding role")
+    .setDescription(`<@&${roleId}> was not successfully given permission ${permission}`)
+}
+
+
+function embedAlreadyHasPermission(permission: string, roleId: string): EmbedBuilder {
+  return failureEmbed
+    .setTitle("Role already has permission")
+    .setDescription(`<@&${roleId}> already has permission ${permission}`)
+}
