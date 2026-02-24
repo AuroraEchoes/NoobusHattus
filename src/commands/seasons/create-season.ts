df61ea5e45a770acc653d16ff046c2982bcdbb6a
AuroraEchoes <auroraesmeralda@protonmail.com>
2026-02-24 18:25:57 +1100

    Add embeds: /create-season

--- src/commands/seasons/create-season.ts
+++ src/commands/seasons/create-season.ts
@@ -1,7 +1,9 @@
 import { ApplicationCommandOptionType } from '@discordjs/core';
-import { Seasons } from '../../db/seasons.js';
+import { SeasonModel, Seasons } from '../../db/seasons.js';
 import type { Command } from '../index.js';
 import { Permission, PermissionManager } from '../../permissions.js';
+import { EmbedBuilder } from 'discord.js';
+import { failureEmbed, successEmbed } from '../../lib/embeds.js';
 
 export default {
   data: {
@@ -22,6 +24,28 @@ export default {
     if (!interaction.isChatInputCommand()) return;
     const seasonName = interaction.options.getString("season-name")!
     const newSeason = await Seasons.create(seasonName)
-    await interaction.reply(`Created Season ${newSeason.season_name} (\`${newSeason.id}\`)`);
+    if (newSeason === undefined) {
+      await interaction.reply({ embeds: [embedFailure(seasonName)], ephemeral: true });
+    } else {
+      await interaction.reply({ embeds: [embedSuccess(newSeason)], ephemeral: true });
+    }
   },
 } satisfies Command;
+
+function embedSuccess(season: SeasonModel): EmbedBuilder {
+  return successEmbed
+    .setTitle("Season created")
+    .addFields([
+      { name: "Season Name", value: `${season.season_name}` },
+      { name: "Season ID", value: `${season.id}` }
+    ])
+}
+
+function embedFailure(seasonName: string): EmbedBuilder {
+  return failureEmbed
+    .setTitle("Could not create season")
+    .setDescription(`Season could not be created.`)
+    .setFields([
+      { name: "Season Name", value: `${seasonName}` }
+    ])
+}
