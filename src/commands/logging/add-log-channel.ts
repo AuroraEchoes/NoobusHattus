795508bca0c243f315ad67a3c14b36ab1536b3f0
AuroraEchoes <auroraesmeralda@protonmail.com>
2026-02-26 12:49:48 +1100

    Add /query-log-channel, improve /add-log-channel

--- src/commands/logging/add-log-channel.ts
+++ src/commands/logging/add-log-channel.ts
@@ -25,11 +25,19 @@ export default {
     const channelParam = interaction.options.getChannel("channel")
     const channelId = channelParam === null ? BigInt(interaction.channelId) : BigInt(channelParam.id)
     const guildId = BigInt(interaction.guildId!);
+
+    const existingLogChannel = await LogChannels.getByGuild(guildId)
+    if (existingLogChannel !== undefined) {
+      await interaction.reply({ embeds: [embedFailureOneLogChannelPerServer(existingLogChannel)], flags: MessageFlags.Ephemeral })
+      return
+    }
+
     const logChannel = await LogChannels.create(channelId, guildId)
     if (logChannel === undefined) {
       await interaction.reply({ embeds: [embedFailureLogChannel(channelId)], flags: MessageFlags.Ephemeral })
       return
     }
+
     await interaction.reply({ embeds: [embedSuccess(logChannel)], flags: MessageFlags.Ephemeral })
   },
 } satisfies Command;
@@ -45,5 +53,12 @@ function embedFailureLogChannel(channelId: bigint): EmbedBuilder {
   return failureEmbed
     .setTitle("Failed to add Log Channel")
     .setDescription(`<#${channelId}> could not be added as a log channel. Is there already a season logging in this channel? This can be checked using \`/list-log-channels\`.`)
+    .setFields([])
+}
+
+function embedFailureOneLogChannelPerServer(existing: LogChannelModel): EmbedBuilder {
+  return failureEmbed
+    .setTitle("There is already a log channel in this server")
+    .setDescription(`Logging is already set up in <#${existing.channel_id}>. There may only be one log channel per server.`)
     .setFields([])
 }
